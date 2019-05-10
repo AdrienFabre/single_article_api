0a2e604c11f4c4e71b7df7b21396114565491012
Adrien Fabre <adrien.fabre.1@gmail.com>
2019-05-10 13:10:54 +0100

    should return 200 ok

---
+++ test/acceptance_tests/articles.test.ts
@@ -0,0 +1,17 @@
+import * as request from "supertest";
+import {app} from "../../src/server";
+
+describe("GET /articles", () => {
+
+  it("SHOULD return 200 Ok", done => {
+  
+    request(app)
+      .get("/articles")
+      .end((err, res) => {
+    expect(res.status).toBe(200);
+      done();
+    });
+
+  });
+
+});
